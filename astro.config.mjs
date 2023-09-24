148e86317b021e19f27aafb2c026202d51541d9c
scherpablo <scherpablo@gmail.com>
2023-09-24 16:27:09 -0300

    build

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   // ...
+  env: {
+    publicPath: process.env.NODE_ENV === 'production' ? '/' : '/public/',
+  },
 });
